8ba323573dc9f93e78133d9c8c2c365b197cdb48
Renato <renatorock3@hotmail.com.br>
2017-08-17 21:32:43 -0300

    Atualizacao

---
+++ router.php
@@ -0,0 +1,35 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+
+
+function rotas_home(){
+
+
+	$app = new \Slim\Slim();
+
+
+	$app->get('/', function(){
+
+		include_once("home_front.php");
+
+	});
+
+
+	$app->get('/viajens', function(){
+
+		include_once("consulta_viajens.php");
+
+	});
+
+
+	$app->run();
+
+
+}
+
+
+
+
+ ?>
