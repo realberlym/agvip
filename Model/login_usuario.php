9b1addf6afc4f003d5a03c1835a3f837fc7fc82b
Renato <renatorock3@hotmail.com.br>
2017-08-19 02:55:20 -0300

    Atualizacao

--- Model/login_usuario.php
+++ Model/login_usuario.php
@@ -2,16 +2,30 @@
 
 $root = $_SERVER["DOCUMENT_ROOT"];
 
-
 require_once($root."agvip/config/config.inc.php");
 
-try {
-    $conn = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+try{
+
+			$mysql = new PDO(DB_TYPE.':host='.DB_HOST.';dbname='.DB_NAME_OPERACOES.';charset=utf8', DB_USER, DB_PASS);
+			$mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			$sql = "SELECT * FROM agvip.usuario where nome = 'Renato'";
+			$stmt = $mysql->prepare($sql);
+			$stmt->execute();
+			$resultado_row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+			print_r($resultado_row);
+		
+
+	}catch(PDOException $e){
+
+
+			 
+			echo $e->getMessage();
+		 
+		
+
+		exit;
+
+
+	}
