1b36556240584bbea0aed1e088dee355f6c2a170
Renato <renatorock3@hotmail.com.br>
2017-08-19 01:54:15 -0300

    Admin

--- Model/login_usuario.php
+++ Model/login_usuario.php
@@ -2,17 +2,3 @@
 
 
 require_once('../config/config.inc.php');
-/*
-
-try {
-    $conn = new PDO("mysql:host="DB_HOST"dbname="DB_NAME, DB_USER, DB_PASSWORD);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully"; 
-    }
-catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
-
-*/
--- config/config.inc.php
+++ config/config.inc.php
@@ -1,5 +1,10 @@
-<?php 
+<?php
 
+version_compare(PHP_VERSION, '5.5.0') <= 0 ? error_reporting(E_WARNING & ~E_NOTICE & ~E_DEPRECATED & E_ERROR) 
+:error_reporting(E_WARNING & ~E_NOTICE & ~E_DEPRECATED  & E_ERROR & ~E_STRICT);
+
+
+define('DB_TYPE', 'mysql');
 define('DB_NAME', 'agvip');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'p@ssw0rd');
