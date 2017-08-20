2495ce7a4f633d76b86d380734a4bcbf09acdb05
Renato <renatorock3@hotmail.com.br>
2017-08-19 21:53:40 -0300

    Banco de Dados

--- Controller/login_usuario.php
+++ Controller/login_usuario.php
@@ -1,7 +1,7 @@
 <?php 
 
 $root = $_SERVER["DOCUMENT_ROOT"];
-require_once($root."agvip/model/login_usuario.php");
+include_once($root."agvip/model/login_usuario.php");
 
 
 function loginUsuario($usuario, $senha){
