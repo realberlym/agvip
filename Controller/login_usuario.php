95a4a4f27831de630134c9e78f33529adfca494e
Renato <renatorock3@hotmail.com.br>
2017-08-22 23:17:52 -0300

    Atualizado

--- Controller/login_usuario.php
+++ Controller/login_usuario.php
@@ -6,6 +6,8 @@ include_once($root."agvip/model/login_usuario.php");
 
 function loginUsuario($usuario, $senha){
 
+	echo $usuario."<br>";
+	echo $senha."<br>";
 
 	/*
 	loginModel($senha_post = $senha, $usuario_post = $usuario_post);
