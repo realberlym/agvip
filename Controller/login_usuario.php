63dac1953d663dd1039df17c8fb5471fd8395b5d
Renato <renatorock3@hotmail.com.br>
2017-08-22 23:21:38 -0300

    Atualizado

--- Controller/login_usuario.php
+++ Controller/login_usuario.php
@@ -1,7 +1,7 @@
 <?php 
 
 $root = $_SERVER["DOCUMENT_ROOT"];
-include_once($root."agvip/model/login_usuario.php");
+include_once($root."agvip/Model/login_usuario.php");
 
 
 function loginUsuario($usuario, $senha){
