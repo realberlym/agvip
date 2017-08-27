f26c8175af7ae014ce5c88bb4e8d468539c647ea
Renato <renatorock3@hotmail.com.br>
2017-08-27 11:21:12 -0300

    Atual

--- AdminLTE-2.3.11/index.php
+++ AdminLTE-2.3.11/index.php
@@ -517,7 +517,36 @@ $host = "http://13.56.14.158";
 
     <!-- Main content -->
     
-    <!--Meio da pagina -->
+    <div class="container">
+  <h2>Hover Rows</h2>
+  <p>The .table-hover class enables a hover state on table rows:</p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
     <!-- /.content -->
   </div>
