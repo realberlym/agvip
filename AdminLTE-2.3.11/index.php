81c7965193a72d15b57042a6a8b2acc16d00c024
Renato <renatorock3@hotmail.com.br>
2017-08-27 11:10:15 -0300

    Atual

--- AdminLTE-2.3.11/index.php
+++ AdminLTE-2.3.11/index.php
@@ -327,7 +327,7 @@ $host = "http://13.56.14.158";
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
         <li class="header">MAIN NAVIGATION</li>
-        <li class="active treeview">
+        <li class="treeview">
           <a href="#">
             <i class="fa fa-dashboard"></i> <span>Produtos</span>
             <span class="pull-right-container">
