3e857f55f500df8312ff9c26324782b78e92e9dd
Renato <renatorock3@hotmail.com.br>
2017-08-30 20:10:09 -0300

    Atualizado

--- AdminLTE-2.3.11/Produtos/cadastro/cad_passagens.php
+++ AdminLTE-2.3.11/Produtos/cadastro/cad_passagens.php
@@ -514,14 +514,96 @@ $host = "http://13.56.14.158";
     </section>
 
     <!-- Main content -->
+     <form action="index.html">
+    <div class="row">
+      <div class="form-group col-md-4">
+        <label for="exampleInputEmail1">Campo Um</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-4">
+        <label for="exampleInputEmail1">Campo Dois</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-4">
+        <label for="exampleInputEmail1">Campo Três</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+  </div>
+  
+  <div class="row">
+      <div class="form-group col-md-3">
+        <label for="exampleInputEmail1">Campo Quatro</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-3">
+        <label for="exampleInputEmail1">Campo Cinco</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-3">
+        <label for="exampleInputEmail1">Campo Seis</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-3">
+        <label for="exampleInputEmail1">Campo Sete</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+  </div>
+  
+  <div class="row">
+      <div class="form-group col-md-6">
+        <label for="exampleInputEmail1">Campo Oito</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-6">
+        <label for="exampleInputEmail1">Campo Nove</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+  </div>
+  
+  <div class="row">
+      <div class="form-group col-md-3">
+        <label for="exampleInputEmail1">Campo Dez</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-3">
+        <label for="exampleInputEmail1">Campo Onze</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-3">
+        <label for="exampleInputEmail1">Campo Doze</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-3">
+        <label for="exampleInputEmail1">Campo Treza</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+  </div>
+  
+  <div class="row">
+      <div class="form-group col-md-6">
+        <label for="exampleInputEmail1">Campo Quatorze</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+    <div class="form-group col-md-6">
+        <label for="exampleInputEmail1">Campo Quinze</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+      </div>
+  </div>
+  
+  <hr />
+  
+  <div class="row">
+    <div class="col-md-12">
+      <button type="submit" class="btn btn-primary">Salvar</button>
+    <a href="template.html" class="btn btn-default">Cancelar</a>
+    </div>
+  </div>
+
+  </form>
 
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <div class="input-group">
-  <span class="input-group-addon" id="basic-addon1">@</span>
-  <input type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
-</div>
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.3.8
