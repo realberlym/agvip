0e6bc42a3c7181fbfc468efdfe1917c9fbc1fecd
Renato <renatorock3@hotmail.com.br>
2017-08-31 00:22:28 -0300

    Ok

--- Controller/cadastrar_produtos.php
+++ Controller/cadastrar_produtos.php
@@ -1,7 +1,7 @@
 <?php 
 
 $root = $_SERVER["DOCUMENT_ROOT"];
-include_once($root."agvip/Model/cadastrar_produtos.php");
+#include_once($root."agvip/Model/cadastrar_produtos.php");
 
 
 function CadPassagens($tipo, $data_ida, $data_volta, $veiculo, $valor, $num_passageiros, $origem, $destino, $opcao_destino){
