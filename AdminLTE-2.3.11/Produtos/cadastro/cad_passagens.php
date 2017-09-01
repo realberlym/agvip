64afe373240cdf702f0a7aabb2353298913b782d
Renato <renatorock3@hotmail.com.br>
2017-08-31 23:24:27 -0300

    Ok Atual

--- AdminLTE-2.3.11/Produtos/cadastro/cad_passagens.php
+++ AdminLTE-2.3.11/Produtos/cadastro/cad_passagens.php
@@ -2,7 +2,8 @@
 
 $link = "http://13.56.14.158/agvip/AdminLTE-2.3.11/";
 $host = "http://13.56.14.158";
-
+  
+$teste = "";
 
 ?>
 <!DOCTYPE html>
@@ -514,7 +515,7 @@ $host = "http://13.56.14.158";
     </section>
 
     <!-- Main content -->
-    <?= ?>
+    <?=$teste?>
      <form class="col-md-10" action="http://13.56.14.158/agvip/salva_passagens" method="POST">
     <div class="row">
       <div class="form-group col-md-4">
