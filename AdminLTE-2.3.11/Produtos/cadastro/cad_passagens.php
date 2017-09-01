1c4174c936b7238928767a38ada6d3c88168c6c2
Renato <renatorock3@hotmail.com.br>
2017-08-31 22:44:27 -0300

    Ok Atual

--- AdminLTE-2.3.11/Produtos/cadastro/cad_passagens.php
+++ AdminLTE-2.3.11/Produtos/cadastro/cad_passagens.php
@@ -537,7 +537,7 @@ $host = "http://13.56.14.158";
   <div class="row">
       <div class="form-group col-md-3">
         <label for="exampleInputText">Veículo</label>
-        <input type="email" class="form-control" id="veiculo" name="veiculo" placeholder="Exemplo: Avião...">
+        <input type="text" class="form-control" id="veiculo" name="veiculo" placeholder="Exemplo: Avião...">
       </div>
     <div class="form-group col-md-3">
         <label for="exampleInputText">Valor</label>
